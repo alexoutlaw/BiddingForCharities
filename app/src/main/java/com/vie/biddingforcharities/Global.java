cd85a41ab80af64b28ab8eb6813ca0cba8cc6409
Alex Outlaw <alexander.outlaw@gmail.com>
2015-02-14 10:23:55 -0700

    Project/Gradle setup App-wide font Prototype login screen

---
+++ app/src/main/java/com/vie/biddingforcharities/Global.java
@@ -0,0 +1,14 @@
+package com.vie.biddingforcharities;
+import android.app.Application;
+
+public class Global extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Set app-wide standard font
+        //stolen from http://stackoverflow.com/questions/2711858/is-it-possible-to-set-font-for-entire-application/16883281#16883281
+        FontsOverride.setDefaultFont(this, "DEFAULT", "fonts/myriadpro_reg.otf");
+        FontsOverride.setDefaultFont(this, "SERIF", "fonts/myriadpro_reg.otf");
+    }
+}
--- app/src/main/java/com/vie/biddingforcharities/LoginActivity.java
+++ app/src/main/java/com/vie/biddingforcharities/LoginActivity.java
@@ -1,39 +1,19 @@
 package com.vie.biddingforcharities;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
 
-
-public class LoginActivity extends ActionBarActivity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        /*Button loginButton = (Button) findViewById(R.id.buttonLogin);
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/myriadpro_reg.otf");
+        loginButton.setTypeface(font);*/
     }
 }
